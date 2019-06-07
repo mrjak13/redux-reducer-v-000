53dbdfc642d497dad02908d9864a08416015a588
Justin Kenney <justinkenney13@gmail.com>
2019-06-07 21:34:18 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+	switch(action.type) {
+		case 'ADD_FRIEND' :
+			return {friends: [...state.friends, action.friend]}
+		case 'REMOVE_FRIEND' :
+			let friendz = state.friends
+
+			function arrayRemove(arr, value) {
+
+			   return arr.filter(function(ele){
+			       return ele.id != value.id;
+			   });
+			}
+
+			let newFriendz = arrayRemove(friendz, action)
+
+			return {friends: newFriendz}
+		default:
+			return state;
+	}
 }
